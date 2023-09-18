319ef87c9c7d7996b24965847e1dedafa44d8092
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-09-19 00:09:14 +0300

    feat: pre lecture 2 homework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeaderModule} from './components/header/header.module';
+import {ProductsListModule} from './pages/products-list/products-list.module';
 
 // Directive
 
@@ -15,7 +16,13 @@ import {HeaderModule} from './components/header/header.module';
 @NgModule({
     declarations: [AppComponent], // const - es6
     exports: [AppComponent], // module.export = {...} - es6
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule], // import {...} from '...module.ts' - es6
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HeaderModule,
+        ProductsListModule,
+    ], // import {...} from '...module.ts' - es6
     bootstrap: [AppComponent],
 })
 export class AppModule {}

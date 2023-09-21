4184503047f0a74ccaa63c1199ffc3528aa576e7
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-09-21 19:17:41 +0300

    feat: preparatory work for lecture 3

---
+++ src/app/shared/application-config/application-config.mock.ts
@@ -0,0 +1,7 @@
+import {IApplicationConfig} from './application-config.interface';
+
+export const applicationConfigMock: IApplicationConfig = {
+    title: 'Angular-learnjs-110923',
+    shopIconSrc: '../../../favicon.ico',
+    headerColorPalete: 'primary',
+};

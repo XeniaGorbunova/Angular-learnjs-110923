6dcd42c365e0bb89497669627f8a7d975b430954
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-09-28 20:43:39 +0300

    feat: view lifecycle hooks

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,4 +1,18 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewChecked,
+    AfterViewInit,
+    Component,
+    DoCheck,
+    EventEmitter,
+    Input,
+    OnChanges,
+    OnDestroy,
+    OnInit,
+    Output,
+    SimpleChanges,
+} from '@angular/core';
 import {IProduct} from '../../../shared/products/product.interface';
 
 @Component({
@@ -6,7 +20,17 @@ import {IProduct} from '../../../shared/products/product.interface';
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.css'],
 })
-export class CardComponent {
+export class CardComponent
+    implements
+        OnChanges,
+        OnInit,
+        DoCheck,
+        AfterContentInit,
+        AfterContentChecked,
+        AfterViewInit,
+        AfterViewChecked,
+        OnDestroy
+{
     @Input() product: IProduct | undefined;
 
     @Output() readonly buy = new EventEmitter<IProduct['_id']>();
@@ -21,4 +45,53 @@ export class CardComponent {
     isStarActive(starIndex: number): boolean {
         return !!(this.product && this.product.rating >= starIndex);
     }
+
+    // --------------------------------------------------------
+
+    constructor() {
+        // eslint-disable-next-line no-console
+        console.log('CardComponent', 'constructor');
+    }
+
+    ngOnChanges(simpleChanges: SimpleChanges) {
+        // eslint-disable-next-line no-console
+        console.log('CardComponent', 'ngOnChanges', simpleChanges);
+    }
+
+    ngOnInit() {
+        // eslint-disable-next-line no-console
+        console.log('CardComponent', 'ngOnInit');
+    }
+
+    ngDoCheck() {
+        // eslint-disable-next-line no-console
+        console.log('CardComponent', 'ngDoCheck');
+    }
+
+    ngAfterContentInit() {
+        // eslint-disable-next-line no-console
+        console.log('CardComponent', 'ngAfterContentInit');
+    }
+
+    ngAfterContentChecked() {
+        // eslint-disable-next-line no-console
+        console.log('CardComponent', 'ngAfterContentChecked');
+    }
+
+    ngAfterViewInit() {
+        // eslint-disable-next-line no-console
+        console.log('CardComponent', 'ngAfterViewInit');
+    }
+
+    ngAfterViewChecked() {
+        // eslint-disable-next-line no-console
+        console.log('CardComponent', 'ngAfterViewChecked');
+    }
+
+    ngOnDestroy() {
+        // eslint-disable-next-line no-console
+        console.log('CardComponent', 'ngOnDestroy');
+    }
+
+    // --------------------------------------------------------
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,15 @@
-import {Component} from '@angular/core';
+import {
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewChecked,
+    AfterViewInit,
+    Component,
+    DoCheck,
+    OnChanges,
+    OnDestroy,
+    OnInit,
+    SimpleChanges,
+} from '@angular/core';
 import {productsMock} from '../../shared/products/products.mock';
 import {IProduct} from '../../shared/products/product.interface';
 
@@ -7,11 +18,71 @@ import {IProduct} from '../../shared/products/product.interface';
     templateUrl: './products-list.component.html',
     styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent {
+export class ProductsListComponent
+    implements
+        OnChanges,
+        OnInit,
+        DoCheck,
+        AfterContentInit,
+        AfterContentChecked,
+        AfterViewInit,
+        AfterViewChecked,
+        OnDestroy
+{
     readonly products = productsMock;
 
     onProductBuy(id: IProduct['_id']) {
         // eslint-disable-next-line no-console
+        // eslint-disable-next-line no-console
         console.log(id);
     }
+
+    // --------------------------------------------------------
+
+    constructor() {
+        // eslint-disable-next-line no-console
+        console.log('ProductsListComponent', 'constructor');
+    }
+
+    ngOnChanges(simpleChanges: SimpleChanges) {
+        // eslint-disable-next-line no-console
+        console.log('ProductsListComponent', 'ngOnChanges', simpleChanges);
+    }
+
+    ngOnInit() {
+        // eslint-disable-next-line no-console
+        console.log('ProductsListComponent', 'ngOnInit');
+    }
+
+    ngDoCheck() {
+        // eslint-disable-next-line no-console
+        console.log('ProductsListComponent', 'ngDoCheck');
+    }
+
+    ngAfterContentInit() {
+        // eslint-disable-next-line no-console
+        console.log('ProductsListComponent', 'ngAfterContentInit');
+    }
+
+    ngAfterContentChecked() {
+        // eslint-disable-next-line no-console
+        console.log('ProductsListComponent', 'ngAfterContentChecked');
+    }
+
+    ngAfterViewInit() {
+        // eslint-disable-next-line no-console
+        console.log('ProductsListComponent', 'ngAfterViewInit');
+    }
+
+    ngAfterViewChecked() {
+        // eslint-disable-next-line no-console
+        console.log('ProductsListComponent', 'ngAfterViewChecked');
+    }
+
+    ngOnDestroy() {
+        // eslint-disable-next-line no-console
+        console.log('ProductsListComponent', 'ngOnDestroy');
+    }
+
+    // --------------------------------------------------------
 }

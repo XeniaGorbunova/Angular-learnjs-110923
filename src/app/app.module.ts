37e778a4b8d47d8a1ca6a67911e4ea3cda87e3d9
Xenia Gorbunova <GorbunovaKsenia@gmail.com>
2023-10-01 17:47:16 +0300

    fix: toggle popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import {AppComponent} from './app.component';
 import {HeaderModule} from './components/header/header.module';
 import {ProductsListModule} from './pages/products-list/products-list.module';
 import {SidenavModule} from './components/sidenav/sidenav.module';
-import {HostPopupModule} from './shared/host-popup/host-popup.module';
+import {HostPopupModule} from './components/host-popup/host-popup.module';
 
 @NgModule({
     declarations: [AppComponent],

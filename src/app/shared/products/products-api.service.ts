8e1c62225e97237fcd55a3f43c2390c403c7c03c
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-10-12 20:20:41 +0300

    feat: pre lecture 9

--- src/app/shared/products/products-api.service.ts
+++ src/app/shared/products/products-api.service.ts
@@ -3,17 +3,22 @@ import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {IProduct} from './product.interface';
 import {baseUrl} from '../base-url/base-url.const';
+import {IProductsDto} from './products.dto';
+import {IProductDto} from './product.dto';
 
-@Injectable({
-    providedIn: 'root',
-})
+@Injectable({providedIn: 'root'})
 export class ProductsApiService {
     constructor(private readonly httpClient: HttpClient) {}
 
     getProducts$(): Observable<IProduct[]> {
-        // return of({data: {items: productsMock}}).pipe(map(({data}) => data.items));
         return this.httpClient
-            .get<{data: {items: IProduct[]}}>(`${baseUrl}/products/suggestion`)
+            .get<IProductsDto>(`${baseUrl}/products`)
             .pipe(map(({data}) => data.items));
+    }
+
+    getProduct$(id: string): Observable<IProduct | undefined> {
+        return this.httpClient
+            .get<IProductDto>(`${baseUrl}/products/${id}`)
+            .pipe(map(({data}) => data));
     }
 }
--- src/app/shared/products/products-store.service.ts
+++ src/app/shared/products/products-store.service.ts
@@ -1,26 +1,39 @@
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject, Observable, filter} from 'rxjs';
 import {Injectable} from '@angular/core';
 import {IProduct} from './product.interface';
 import {ProductsApiService} from './products-api.service';
 
-@Injectable({
-    providedIn: 'root',
-})
+@Injectable({providedIn: 'root'})
 export class ProductsStoreService {
     private readonly productsStore$ = new BehaviorSubject<IProduct[] | null>(null);
+    private readonly currentProductStore$ = new BehaviorSubject<IProduct | null>(null);
 
     get products$(): Observable<IProduct[] | null> {
         return this.productsStore$.asObservable();
     }
 
+    get currentProduct$(): Observable<IProduct | null> {
+        return this.currentProductStore$.asObservable();
+    }
+
     constructor(private readonly productsApiService: ProductsApiService) {}
 
     loadProducts() {
-        // setTimeout(() => {
-        //     this.productsStore$.next(productsMock);
-        // }, 2000);
-        this.productsApiService.getProducts$().subscribe(value => {
-            this.productsStore$.next(value);
+        this.productsApiService.getProducts$().subscribe(products => {
+            this.productsStore$.next(products);
         });
+    }
+
+    loadProduct(productId: string) {
+        const productPreview = this.productsStore$.value?.find(({_id}) => _id === productId);
+
+        this.currentProductStore$.next(productPreview || null);
+
+        this.productsApiService
+            .getProduct$(productId)
+            .pipe(filter(Boolean))
+            .subscribe(product => {
+                this.currentProductStore$.next(product);
+            });
     }
 }
